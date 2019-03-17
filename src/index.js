4365d0f4fffc3c27d4cfa85e6768e3926e875203
darya.gembitskaya <daxagemb@gmail.com>
2019-03-17 18:55:20 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if(currency === 0) {
+        return {};
+    }
+    else if(currency > 10000){
+        return {"error: \"You are rich, my friend! We don't have so much coins for exchange\""};
+    }
+    else{
+        let count;
+        if(currency/50 !== 0) {
+            count.put("\"H\"" + ":" + currency/50);
+
+        }
+        currency %= 50;
+        if(currency/25 !== 0) {
+            count.put(",\"Q\"" + ":" + currency/25);
+
+        }
+        currency %= 25;
+        if(currency/10 !== 0) {
+            count.put("\"D\"" + ":" + currency/10);
+
+        }
+        currency %= 10;
+        if(currency/5 !== 0) {
+            count.put("\"N\"" + ":" + currency/5);
+
+        }
+        currency %= 5;
+        if(currency !== 0) {
+            count.put("\"H\"" + ":" + currencynp);
+
+        }
+        return {count};
+    }
+
 }
